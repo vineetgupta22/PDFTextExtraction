624d028583c259b26042d13e181275d5d9b886d2
Vineet Gupta <vineetgupta22@gmail.com>
2015-01-29 21:09:14 +0530

    Inital Procject Commit.
    Signed-off-by: Vineet Gupta <vineetgupta22@gmail.com>

---
+++ src/Main.c
@@ -0,0 +1,26 @@
+/**
+*	Copyright (c) 2008-2015, Cee Emm Infotech and/or its affiliates.
+*	All rights are reserved.
+*
+*	Program Name:		PDFTextExtraction
+*	Purpose of Program:	Extraction of Text from PDF Files
+*	Program Owner:		Cee Emm Infotech
+*	Chief Developer:	Vineet Gupta
+*	
+*	contact: vineetgupta22@gmail.com
+*	Address: Cee Emm Infotech, 605, Sector 10D, Chandgiarh [INDIA]
+*
+**/
+
+
+/**
+*	@file		:	Main.c
+*	@brief		:	Exe Project 
+**/
+
+	#include <stdio.h>
+
+	int main(int argc __attribute__ ((unused)), char **argv __attribute__ ((unused))){
+		printf("Starting things");
+		return 0;
+	}
